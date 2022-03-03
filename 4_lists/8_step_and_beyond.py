80170373c4b38af87f98bbc0c97a4e389f6f746b
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-03-03 20:27:10 +0300

    Практика

---
+++ 4_lists/8_step_and_beyond.py
@@ -0,0 +1,49 @@
+# # Python: коллекции (индексирование, срезы, сортировка)
+# # Статья https://habr.com/ru/post/319200/
+
+
+# # С помощью срезов можно менять значения эл.,
+# # удалять и добавлять новые эл.
+
+# # Задача со stepik
+
+# numbers = [4, 8, 12, 16, 34, 56, 100]
+# numbers[1:4] = [20, 24, 28]
+# # print(numbers) # 4, 20, 24, 28, 34, 56, 100
+
+# # Lst[ Start(included) : End(not included) : Step ]
+# # By default: Lst[ 0 : length of list(last el included) : 1]
+
+# numbers = [5, 10, 15, 25]
+# print(numbers[::-2]) #
+# print(numbers[0:4:-2]) #
+
+# # Почему результирующий срез - разный?
+
+
+from multiprocessing.sharedctypes import copy
+
+
+numbers = [5, 10, 15, 25]
+# print(numbers[::-2])
+
+
+# Создание копии списка
+
+letters = ['a', 'b', 'c', 'd']
+
+copied_letters = letters.copy()
+# print(copied_letters)
+
+
+# Соединение эл. списка в строку
+
+words = ['Hello', 'Python']
+# print('-'.join(words))
+
+
+# numbers = [10, 20, 30, 40]
+# del numbers[0:1]
+
+# print(numbers)
+
---
+++ 4_lists/main.py
@@ -0,0 +1,41 @@
+# list1 = ['a', 'b', 'c', 'd']
+# list2 = ['e', 'f', 'g']
+
+# # ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+# list3 = list1 + list2
+# # print(list3)
+
+
+# zeros = [0] * 10
+# # print(zeros)
+
+
+# numbers = [10, 20, 30, 40, 50]
+# print(numbers)
+# # print(numbers[-2]) # by index
+
+# # Python lists slicing
+# # slicing operator is :
+
+# # Lst[ Start(included) : End(not included) : Step ]
+# # By default: Lst[ 0 : length of list(last el included) : 1]
+
+# List = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ 
+# print("\nOriginal List:\n", List)
+# print("\nSliced Lists: ")
+# print(List[3:9:2])
+# print(List[::2])
+# print(List[::])
+
+# print("qazsexbjk")
+
+# wordsList = ["a", "b", "c"]
+
+# print(wordsList[0:3:-3]) # why c?
+
+# numbers = [10, 20, 30, 40, 50]
+# print(numbers[-2]) # 40
+# print(numbers[-4:-1]) # от 20 до 50 с шагом 1
+
